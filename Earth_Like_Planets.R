f672ce9dc8b87f2e7a3d803021f70b7ec7653e01
rngaw@hotmail.com <rngaw@hotmail.com>
2016-09-25 20:25:46 -0700

    Starting Project

---
+++ Earth_Like_Planets.R
@@ -0,0 +1,11 @@
+#Finding Planets like Earth and Methods to Get There
+
+#Information about the sun is gathered here: 
+#https://en.wikipedia.org/wiki/Sun
+
+#Visual Brightness = -26.74
+
+#Absolute Magnitude = 4.83
+
+#Spectral Classification = G2V
+
